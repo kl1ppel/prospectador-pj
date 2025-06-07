c78d00d8bc9094e96d3e163c215402c6ff307e2e
Gabriel K <gabrielkklippel@outlook.com>
2025-06-07 00:06:17 -0300

    docs: note file transfer via Firebase

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,13 +1,16 @@
-import app, { auth, db } from './config';
+import app, { auth, db, storage } from './config';
 import firebaseAuthService from './authService';
 import firebaseMessageService from './messageService';
 import firebaseRdStationService from './rdStationService';
+import firebaseFileTransferService from './fileTransferService';
 
 export {
   app,
   auth,
   db,
+  storage,
   firebaseAuthService,
   firebaseMessageService,
-  firebaseRdStationService
+  firebaseRdStationService,
+  firebaseFileTransferService
 };
